83930a071278f5c6260df2608e85ba34394c8628
Ozan YAPRAK <ozanerhanyaprak@icloud.com>
2023-09-02 11:05:20 +0300

    Final

---
+++ CityApp.API/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CityApp.API.Models;
+using CityApp.API.Models.Dto.City;
+
+namespace CityApp.API
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<City, GetCity>();
+            CreateMap<PostCity, City>();
+        }
+    }
+}
